238a91262db8a4a6caa54fdd16552d5ab370102c
Shawn Inder <shawn@vigour.io>
2017-10-29 16:05:46 -0400

    Remove styles

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import './styles.scss'
-
 const Message = (props) => {
   const classes = props.className.split(' ')
   classes.push('message')
